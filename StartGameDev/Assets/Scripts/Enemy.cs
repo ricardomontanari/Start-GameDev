f159579f49de45e105532dab1c3e1d5378f99dc4
Ricardo Montanari <ricardo.montanari@outlook.com>
2023-01-22 21:17:18 -0300

    Adição das classes Player e Enemy

---
+++ StartGameDev/Assets/Scripts/Enemy.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int health;
+    public float speed;
+    
+    void Movement()
+    {
+
+    }
+    void SeekPlayer()
+    {
+        
+    }
+    void FollowPlayer()
+    {
+        
+    }
+    void Attack()
+    {
+        
+    }
+}
---
+++ StartGameDev/Assets/Scripts/Player.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public int health;
+    public float speed;
+
+    void Attack()
+    {
+
+    }
+    void Movement()
+    {
+        
+    }
+    void Jump()
+    {
+        
+    }
+}
